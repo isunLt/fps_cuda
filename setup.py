7da92283ced2dd44cc6b65405df7ff9a438d70e9
Tianfang-Sun <37700210+isunLt@users.noreply.github.com>
2021-05-06 21:45:58 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ exec(open(osp.join("pointnet2", "_version.py")).read())
 setup(
     name="pointnet2",
     version=__version__,
-    author="Erik Wijmans",
+    author="isunLt",
     packages=find_packages(),
     install_requires=requirements,
 )
